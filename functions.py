8199620897f43c719b3fa3b63ac4fa18a27771d2
Inventor853 <66274880+Inventor853@users.noreply.github.com>
2021-05-01 09:35:24 -0500

    lesson basics 2

---
+++ functions.py
@@ -0,0 +1,24 @@
+# Today, we will focus on the implementation of a function
+
+# Classic function
+def foo():
+    print('run')
+    
+foo()
+
+# Arguments in a Function
+def arguments(num1, num2):
+    print(num1 + num2)
+
+arguments(3, 5)
+
+# Return answer
+def arguments(num1, num2):
+    return num1 + num2
+
+answer = arguments(3, 5)
+print(answer)
+
+# Class functions
+string = 'india'
+print(string.capitalize())
---
+++ modules.py
@@ -0,0 +1,5 @@
+# This lesson will discuss what a module is, and we will list some examples
+
+from requests import get
+
+print(get('https://www.google.com/'))
